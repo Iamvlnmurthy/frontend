f9c237e9e7e0c8c37cd5e961d05be28a9633698f
iamvlnmurthy <iamvlnmurthy@gmail.com>
2025-06-29 16:36:57 +0530

    refactor: split ContactList and ConfirmationModal into components, move Icons to utils

--- src/utils/icons.jsx
+++ src/utils/icons.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 export const Icons = {
     List: <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-5 h-5 mr-2"><line x1="8" x2="21" y1="6" y2="6"/><line x1="8" x2="21" y1="12" y2="12"/><line x1="8" x2="21" y1="18" y2="18"/><line x1="3" x2="3.01" y1="6" y2="6"/><line x1="3" x2="3.01" y1="12" y2="12"/><line x1="3" x2="3.01" y1="18" y2="18"/></svg>,
     Plus: <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-5 h-5 mr-2"><path d="M12 5L12 19"/><path d="M5 12L19 12"/></svg>,
