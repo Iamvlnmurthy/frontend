8b739f7cd9d4b1a6b88a8877ee7b30e010492ab3
iamvlnmurthy <iamvlnmurthy@gmail.com>
2025-06-29 15:10:11 +0530

    changed api url

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ const Icons = {
     Home: <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-8 h-8 mr-2"><path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/><polyline points="9 22 9 12 15 12 15 22"/></svg>,
 };
 
-const API_BASE_URL = 'http://localhost:3001/api'; // Your Node.js backend URL
+const API_BASE_URL = 'https://backend-ia06.onrender.com'; // Your Node.js backend URL
 
 // Data for Indian States and Districts
 const indianStatesAndDistricts = {
