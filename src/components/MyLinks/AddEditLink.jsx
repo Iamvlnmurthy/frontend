02385c818442371c3a968ac9c2dc3a0446bc4760
iamvlnmurthy <iamvlnmurthy@gmail.com>
2025-06-29 17:45:23 +0530

    changes in Mylink

--- src/components/MyLinks/AddEditLink.jsx
+++ src/components/MyLinks/AddEditLink.jsx
@@ -105,31 +105,33 @@ const AddEditLink = ({ editingLink, setEditingLink, setActiveSubMenu, displayMes
                         value={formData.description}
                         onChange={handleChange}
                         className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-red-500 focus:border-red-500 sm:text-sm resize-y"
-                        placeholder="Optional description for this link..."
+                        placeholder="description for this link..."
                         rows="2"
                     />
                 </div>
-                <div>
-                    <label htmlFor="username" className="block text-sm font-medium text-gray-700">Username:</label>
-                    <input
-                        type="text"
-                        id="username"
-                        value={formData.username}
-                        onChange={handleChange}
-                        className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-red-500 focus:border-red-500 sm:text-sm"
-                        placeholder="Optional username for this link..."
-                    />
-                </div>
-                <div>
-                    <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password:</label>
-                    <input
-                        type="text"
-                        id="password"
-                        value={formData.password}
-                        onChange={handleChange}
-                        className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-red-500 focus:border-red-500 sm:text-sm"
-                        placeholder="Optional password for this link..."
-                    />
+                <div className="flex gap-4">
+                    <div className='grow'>
+                        <label htmlFor="username" className="block text-sm font-medium text-gray-700">Username:</label>
+                        <input
+                            type="text"
+                            id="username"
+                            value={formData.username}
+                            onChange={handleChange}
+                            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-red-500 focus:border-red-500 sm:text-sm"
+                            placeholder="username for this link..."
+                        />
+                    </div>
+                    <div className='grow'>
+                        <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password:</label>
+                        <input
+                            type="text"
+                            id="password"
+                            value={formData.password}
+                            onChange={handleChange}
+                            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-red-500 focus:border-red-500 sm:text-sm"
+                            placeholder="password for this link..."
+                        />
+                    </div>
                 </div>
                 <button
                     type="submit"
